a7a542b17685c2191ebba0a88d091ae18022bdd2
Zachary <zachary.zboncak@gmail.com>
2020-02-08 15:45:38 -0600

    added boilerplate for connecting to a pg database

--- src/server.js
+++ src/server.js
@@ -1,5 +1,19 @@
 const app = require('./app');
-const { PORT } = require('./config');
+const { PORT/*, DB_URL*/ } = require('./config');
+
+/* 
+Use this if connecting to a database
+Be Sure to update the DB_URL in the config.js file 
+*/
+/***************************************/
+// const db = knex({
+//   client: 'pg',
+//   connection: DB_URL,
+// });
+
+// app.set('db', db);
+/***************************************/
+
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
